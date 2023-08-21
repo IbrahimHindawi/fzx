8b47955503238e0bc36ddaa6f010125ac870617b
IbrahimHindawi <ihindawi@gmail.com>
2023-08-21 15:34:54 +0400

    disabled vcpkg in ccompile.bat script

--- scripts/bbuild.bat
+++ scripts/bbuild.bat
@@ -3,7 +3,7 @@
 mkdir build
 pushd build
 
-cmake .. "-DCMAKE_TOOLCHAIN_FILE=C:\devel\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Debug" -G "Visual Studio 17 2022"
+cmake .. "-DCMAKE_TOOLCHAIN_FILE=C:\devel\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Debug"
 rem cmake .. "-DCMAKE_TOOLCHAIN_FILE=C:\devel\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Release"
 powershell -Command ..\scripts\clang-build.ps1 -export-jsondb
 rem python ..\scripts\finalize.py
--- scripts/ccompile.bat
+++ scripts/ccompile.bat
@@ -1,3 +1,3 @@
 @echo off
-msbuild build\fzx.sln -nologo -m -v:m /property:Configuration=Debug
+msbuild build\fzx.sln -nologo -m -v:m /property:Configuration=Debug /property:VcpkgEnabled=false
 rem msbuild build\fzx.sln -nologo -m -v:m /property:Configuration=Release
