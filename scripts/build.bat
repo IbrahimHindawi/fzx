107c952afddac4da7d87bc4082aba67a722cac7e
IbrahimHindawi <ihindawi@gmail.com>
2023-08-21 23:23:18 +0400

    updated README + CMakeToolChainFile var in build.bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,14 +2,15 @@
 
 rem WARNING:
 rem Must be executed from project root directory using `scripts\bbuild.bat`
-rem
 rem TODO(Ibrahim): Implement Debug and Release to CompileCommand.
 
-rem Setup Script Variables
-for %%I in (.) do set projectName=%%~nxI
-rem echo %projectName%
+rem UserParameters:
+set CMakeToolchainFile="C:\devel\vcpkg\scripts\buildsystems\vcpkg.cmake"
 
-set CompileCommand=msbuild build\%projectName%.sln -nologo -m -v:m /property:Configuration=Debug /property:VcpkgEnabled=false
+rem Setup Script Variables
+for %%I in (.) do set ProjectName=%%~nxI
+rem echo %ProjectName%
+set CompileCommand=msbuild build\%ProjectName%.sln -nologo -m -v:m /property:Configuration=Debug /property:VcpkgEnabled=false
 
 rem Program Begin
 echo Use -h to display available commands.
@@ -21,13 +22,14 @@ echo -b to build solution.
 echo -c to compile. 
 echo -cr to compile and run.
 echo -r to run exe.
+echo -s to open solution.
 echo -x to clean up.
 goto :eof
 
 :Build
 mkdir build
 pushd build
-cmake .. "-DCMAKE_TOOLCHAIN_FILE=C:\devel\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Debug" "-DProjectNameParam:STRING=%projectName%"
+cmake .. "-DCMAKE_TOOLCHAIN_FILE=%CMakeToolchainFile%" "-DCMAKE_BUILD_TYPE=Debug" "-DProjectNameParam:STRING=%ProjectName%"
 powershell -Command ..\scripts\clang-build.ps1 -export-jsondb
 popd build
 goto :eof
@@ -38,16 +40,20 @@ goto :eof
 
 :CompileRun
 %CompileCommand%
-build\Debug\%projectName%.exe
+build\Debug\%ProjectName%.exe
 goto :eof
 
 :Run
-build\Debug\%projectName%.exe
+build\Debug\%ProjectName%.exe
 goto :eof
 
 :CleanUp
 echo Destroy build folder.
 rmdir /S /Q build
+goto :eof
+
+:Solution
+build\%ProjectName%.sln
 goto :eof
 
 :GETOPTS
@@ -56,6 +62,7 @@ if /I "%1" == "-b" call :Build
 if /I "%1" == "-c" call :Compile
 if /I "%1" == "-cr" call :CompileRun
 if /I "%1" == "-r" call :Run
+if /I "%1" == "-s" call :Solution
 if /I "%1" == "-x" call :CleanUp
 shift
 if not "%1" == "" call :Epilogue
